8e94fd2677fab374b14a090b3ac8dcd810262b98
Hồ Đình Kha <khahd@DTL.CANH>
2025-11-24 11:20:54 +0700

    update code

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-console.log("?");
+const toggle = document.getElementById('toggleTheme');
+toggle.addEventListener('click', () => {
+    document.documentElement.classList.toggle('light');
+    if (document.documentElement.classList.contains('light')) {
+        document.body.style.background = 'linear-gradient(180deg,#fef6e7,#f0f3ff)';
+        document.body.style.color = '#07202b';
+        toggle.textContent = 'Chế độ sáng';
+    } else {
+        document.body.style.background = '';
+        document.body.style.color = '';
+        toggle.textContent = 'Chế độ tối';
+    }
+})
